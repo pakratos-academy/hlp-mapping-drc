e4b8bc197a640de784922d3f2f05cd047b65b1a9
majorluvale <majorluvale2012@gmail.com>
2024-07-19 11:50:51 +0200

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,5 +1,5 @@
 // paste in your published Google Sheets URL from the browser address bar
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1_c_GTJEQ8iGhdYwwPlU4ff2zXn8DYJsIEHlfEusx6-Y/edit#gid=0';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/14-FkzzROf5z0OlGp_hVnUUuF2VAdcZpUmutvvaa05Js/edit?gid=0#gid=0';
 
 // insert your own Google Sheets API key from https://console.developers.google.com
 var googleApiKey = 'AIzaSyA-kJTKIifwjv9Y3qAId3eaxT32qZkhvN0';
